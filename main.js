56af7ecbf34d58b1ece1d60a3f27f4110aa34b82
Annanski33 <annanski@outlook.com>
2020-06-17 19:09:05 -0400

    svg added and smooth scroll

---
+++ main.js
@@ -0,0 +1,29 @@
+// JQuery Smooth Scroll
+$('.navigation a').on('click', function(e) {
+    if (this.hash !=='') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate( {
+            scrollTop: $(hash).offset().top
+        }, 
+        800
+        );
+    }
+});
+
+$('.header a').on('click', function(e) {
+    if (this.hash !=='') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate( {
+            scrollTop: $(hash).offset().top
+        }, 
+        800
+        );
+    }
+});
+
